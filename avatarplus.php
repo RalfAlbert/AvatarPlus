68338c307944d2e92db75f0f569cb997a0fcfd28
Ralf Albert <neun12@gmail.com>
2013-02-17 11:59:26 +0100

    update version number; fix problem with wordpress update

--- avatarplus.php
+++ avatarplus.php
@@ -9,7 +9,7 @@
  * @subpackage AvatarPlus
  * @author     Ralf Albert <me@neun12.de>
  * @license    GPLv3 http://www.gnu.org/licenses/gpl-3.0.txt
- * @version    0.2.1
+ * @version    0.3
  * @link       http://wordpress.com
  */
 
@@ -17,7 +17,7 @@
  * Plugin Name:	AvatarPlus
  * Plugin URI:	http://yoda.neun12.de
  * Description:	Replacing the standard avatar in comments with a Google+, Facebook or Twitter avatar if a user enter a profile url
- * Version: 	0.2.1
+ * Version: 	0.3
  * Author: 		Ralf Albert
  * Author URI: 	http://yoda.neun12.de
  * Text Domain: avatarplus
